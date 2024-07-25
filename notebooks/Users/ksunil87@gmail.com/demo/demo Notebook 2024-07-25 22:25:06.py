dbd40df2d90806fe5d7b82424ecc0eff893f1251
Sunil Kumar <contact.skr07@gmail.com>
2024-07-25 22:44:11 +0530

    successful demo run

--- notebooks/Users/ksunil87@gmail.com/demo/demo Notebook 2024-07-25 22:25:06.py
+++ notebooks/Users/ksunil87@gmail.com/demo/demo Notebook 2024-07-25 22:25:06.py
@@ -1,1 +1,15 @@
 # Databricks notebook source
+#This notebook creates a dataframe with some sample data that can be used for a quick visualization.
+
+data = [[1, "Elia"], [2, "Teo"], [3, "Fang"]]
+
+# COMMAND ----------
+
+sdf = spark.createDataFrame(data, schema="id LONG, name STRING")
+
+# COMMAND ----------
+
+display(sdf)
+
+# COMMAND ----------
+
